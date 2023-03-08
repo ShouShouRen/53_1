7f7615033ae875d6ad7e68732272000c8b8e0cc0
Peter Chen <peterchen2017125@gmail.com>
2023-03-08 16:15:57 +0800

    20230308

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>咖啡商品展示系統-會員登入</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="position-relative">
+            <div class="d-center">
+                <div class="col-12 p-5 bg-light rounded-lg">
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                        <img src="images/logos.png" class="w-25" alt="">
+                        <h2 class="p-3">咖啡商品展示系統</h2>
+                    </div>
+                    <form class="p-4" action="login_check.php" method="post">
+                        <div>
+                            <label for="">帳號</label>
+                            <input type="text" name="user" class="form-control my-2" require>
+                        </div>
+                        <div>
+                            <label for="">密碼:</label>
+                            <input type="password" name="pw" class="form-control my-2" require>
+                        </div>
+                        <div class="my-2">
+                            <label for="">驗證碼:</label>
+                        </div>
+                        <div class="row justify-content-between mx-1 my-4">
+                            <div class="btn btn-outline-dark">重新產生</div>
+                            <input type="submit" class="btn btn-dark" value="確認登入">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
