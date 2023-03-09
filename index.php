8568c2221494a36c28c438884388b2e1f292c535
Peter Chen <peterchen2017125@gmail.com>
2023-03-09 22:11:07 +0800

    20230309

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
+<?php
+    require_once("pdo.php");
+    session_start();
+    if(!isset($_SESSION["AUTH"])){
+        header("Location: login.php");
+    }
+    try{
+        extract($_POST);
+        // $sql = "SELECT * FROM products ORDER BY products.time DESC";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt->execute();
+        // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-Hant-tw">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>商店首頁</title>
 </head>
+
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand">
+                <img src="./images/logos.png" class="logo mx-3" alt="">
+                <span>咖啡商品展示系統</span>
+            </a>
+        </div>
+    </nav>
 </body>
+
 </html>
--- login_check.php
+++ login_check.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
 
+$max_attempts = 3;
+if (!isset($_SESSION['attempts'])) {
+    $_SESSION['attempts'] = 0;
+}
+if ($_SESSION['attempts'] === $max_attempts) {
+    $_SESSION['attempts'] = 0;
+    header("Location: login_failed.php");
+    exit;
+}
 try {
     require_once("pdo.php");
     extract($_POST);
@@ -9,22 +18,26 @@ try {
     $stmt->execute([$user]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$row) {
+        $_SESSION['attempts']++;
         $_SESSION['error'] = 'account';
         header("Location: login.php");
         exit;
     } else {
         if ($pw != $row['pw']) {
+            $_SESSION['attempts']++;
             $_SESSION['error'] = 'password';
             header("Location: login.php");
             exit;
         } else {
             if ($_POST['captcha'] !== $_SESSION['captcha']) {
+                $_SESSION['attempts']++;
                 $_SESSION['error'] = 'captcha';
                 header("Location: login.php");
                 exit;
             }
+            unset($_SESSION['attempts']);
             $_SESSION["AUTH"] = $row;
-            header("Location:login_check_2.php");
+            header("Location: login_check_2.php");
             exit;
         }
     }
