73becf93ccd4e26e366819c2d55c9317fd32ed8e
Peter Chen <peterchen2017125@gmail.com>
2023-03-09 21:15:27 +0800

    20230309

--- login.php
+++ login.php
@@ -17,37 +17,39 @@ session_start();
     <div class="container">
         <div class="position-relative">
             <div class="d-center">
-                <div class="col-12 p-5 bg-light rounded-lg">
+                <div class="col-12 p-5 rounded-lg shadow-lg text-light" style="backdrop-fliter: bluer(500px); background-color: rgba(255,255,255,.1) ">
                     <div class="d-flex flex-column justify-content-center align-items-center">
                         <img src="images/logos.png" class="w-25" alt="">
                         <h2 class="p-3">咖啡商品展示系統</h2>
                     </div>
                     <form class="p-4" action="login_check.php" method="post">
-                        <div>
-                            <label for="">帳號</label>
-                            <input type="text" name="user" class="form-control my-2" required>
+                        <div class="my-1">
+                            <label for="">帳號:</label>
+                            <input type="text" name="user" class="form-control my-3" required>
                             <?php if (isset($_SESSION['error']) && $_SESSION['error'] === 'account'): ?>
-                            <div class="text-danger">帳號有誤</div>
+                            <div class="text-danger font-weight-bolder">帳號有誤</div>
                             <?php endif; ?>
                         </div>
-                        <div>
+                        <div class="my-1">
                             <label for="">密碼:</label>
-                            <input type="password" name="pw" class="form-control my-2" required>
+                            <input type="password" name="pw" class="form-control my-3" required>
                             <?php if (isset($_SESSION['error']) && $_SESSION['error'] === 'password'): ?>
-                            <div class="text-danger">密碼有誤</div>
+                            <div class="text-danger font-weight-bolder">密碼有誤</div>
                             <?php endif; ?>
                         </div>
-                        <div class="my-2">
-                            <label for="">驗證碼:</label>
-                            <input type="text" name="captcha" class="form-control my-2" required>
+                        <div class="my-1">
+                            <div class="d-flex align-items-center">
+                                <label for="">驗證碼:</label>
+                                <img src="captcha.php" class="ml-3" alt="">
+                            </div>
+                            <input type="text" name="captcha" class="form-control my-3" required>
                             <?php if (isset($_SESSION['error']) && $_SESSION['error'] === 'captcha'): ?>
-                            <div class="text-danger">驗證碼有誤</div>
+                            <div class="text-danger font-weight-bolder">驗證碼有誤</div>
                             <?php endif; ?>
-                            <img src="captcha.php" alt="">
                         </div>
                         <div class="row justify-content-between mx-1 my-4">
-                            <div class="btn btn-outline-dark" id="refresh">重新產生</div>
-                            <input type="submit" class="btn btn-dark" value="確認登入">
+                            <div class="btn btn-outline-light" id="refresh">重新產生</div>
+                            <input type="submit" class="btn btn-light" value="確認登入">
                         </div>
                     </form>
                     <?php
---
+++ login_check_2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-tw">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        td {
+            width: 180px;
+            height: 180px;
+            border: 2px solid #fff;
+            text-align: center;
+            font-size: 24px;
+            cursor: pointer;
+        }
+    </style>
+    <title>二次驗證</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-center text-center d-flex flex-column justify-content-center align-items-center">
+            <table>
+                <tr>
+                    <td data-id="1"></td>
+                    <td data-id="2"></td>
+                </tr>
+                <tr>
+                    <td data-id="3"></td>
+                    <td data-id="4"></td>
+                </tr>
+            </table>
+            <p class="py-3 m-0 text-white">請選擇兩個相鄰的格子，以連成一條水平或垂直線。</p>
+            <button id="validate" class="btn btn-outline-light">驗證</button>
+        </div>
+    </div>
+</body>
+<script src="./js/jquery-3.6.3.min.js"></script>
+<script src="./js/function.js"></script>
+
+</html>
