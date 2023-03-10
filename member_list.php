18edd6c6321811391c177c5c73cd7ad09b7ce80e
Peter Chen <peterchen2017125@gmail.com>
2023-03-10 23:05:11 +0800

    20230310

--- member_list.php
+++ member_list.php
@@ -8,6 +8,7 @@
         $sql = "SELECT * FROM users";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }catch(PDOException $e){
         echo $e->getMessage();
     }
@@ -48,7 +49,7 @@
         </div>
     </nav>
     <div class="container" style="margin-top:86px;">
-        <div class="pt3 pb-5">
+        <div class="pt-3 pb-5">
             <div class="row justify-content-between align-items-center">
                 <h5 class="text-center text-white border-start font-weight-bolder">會員管理</h5>
                 <div class="d-flex justify-content-around align-items-center text-white py-3 w-25">
@@ -60,8 +61,85 @@
             </div>
             <div class="p-4 bg-white rounded-lg shadow-lg">
                 <div class="row justify-content-between align-items-center mb-3">
-                    <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#adduser">新增使用者</button>
+                    <div class="col-6">
+                        <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#adduser">新增使用者</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="adduser">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="adduserLabel">新增使用者</h5>
+                                        <button class="close" data-dismiss="modal">
+                                            <span aria-hidden="true">&times</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="container-sm px-5 py-4">
+                                            <form action="register_store.php" method="post">
+                                                <label for="">帳號</label>
+                                                <input type="text" name="user" class="form-control my-2" require>
+                                                <label for="">使用者姓名</label>
+                                                <input type="text" name="user_name" class="form-control my-2" require>
+                                                <label for="">密碼</label>
+                                                <input type="password" name="pw" class="form-control my-2" require>
+                                                <div class="text-right"><input type="submit" value="註冊" class="btn btn-success"></div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <form action="search_member.php" id="search-member" class="d-flex justify-content-end align-items-center">
+                        <div class="d-flex px-2">
+                            <label for="">升冪</label>
+                            <input type="radio" name="use" id="" value="up">
+                        </div>
+                        <div class="d-flex px-2">
+                            <label for="">降冪</label>
+                            <input type="radio" name="use" id="" value="down">
+                        </div>
+                        <input type="search" name="search" id="search-input" placeholder="請輸入使用者資料"
+                            class="form-control w-50 mr-2">
+                        <button type="submit" class="btn btn-secondary">查詢</button>
+                        </form>
+                    </div>
                 </div>
+                <table class="table">
+                    <tr>
+                        <th>使用者編號</th>
+                        <th>使用者帳號</th>
+                        <th>使用者密碼</th>
+                        <th>使用者名稱</th>
+                        <th>使用者權限</th>
+                        <th>操作</th>
+                    </tr>
+                    <tbody id="search_result">
+
+                    </tbody>
+                    <?php foreach($result as $row) { ?>
+                        <tr class="show-all">
+                            <td><?= $row["user_id"]; ?></td>
+                            <td><?= $row["user"]; ?></td>
+                            <td><?= $row["pw"]; ?></td>
+                            <td><?= $row["user_name"]; ?></td>
+                            <td><?php switch ($row["role"]) { case 0: echo "管理員"; break; case 1: echo "一般使用者"; break; } ?></td>
+                            <td>
+                                <?php if ($row["id"] == 1) { ?>
+                                <?php } elseif ($row["id"] == $_SESSION["AUTH"]["id"]) { ?>
+                                <span class="text-secondary">切換權限</span>
+                                <?php } else { ?>
+                                    <a class="btn btn-outline-secondary" href="switch_role.php?role=<?= $row["role"]; ?>&id=<?= $row["id"]; ?>">權限修改</a>
+                                <?php } ?>
+                                <?php if ($row["id"] == 1) { ?>
+                                    <!-- 隱藏修改的連結 -->
+                                <?php } else { ?>
+                            <?php } ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </table>
             </div>
         </div>
     </div>
