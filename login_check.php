8a028824fd76f9384b0f9b73a4f4afe7d3bb2b1d
Peter Chen <peterchen2017125@gmail.com>
2023-03-10 09:41:09 +0800

    20230310

--- login_check.php
+++ login_check.php
@@ -1,4 +1,48 @@
 <?php
+// session_start();
+// $max_attempts = 3;
+// if (!isset($_SESSION['attempts'])) {
+//     $_SESSION['attempts'] = 0;
+// }
+// if ($_SESSION['attempts'] === $max_attempts) {
+//     $_SESSION['attempts'] = 0;
+//     header("Location: login_failed.php");
+//     exit;
+// }
+// try {
+//     require_once("pdo.php");
+//     extract($_POST);
+//     $sql = "SELECT * FROM users WHERE user = ?";
+//     $stmt = $pdo->prepare($sql);
+//     $stmt->execute([$user]);
+//     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+//     if (!$row) {
+//         $_SESSION['attempts']++;
+//         $_SESSION['error'] = 'account';
+//         header("Location: login.php");
+//         exit;
+//     } else {
+//         if ($pw != $row['pw']) {
+//             $_SESSION['attempts']++;
+//             $_SESSION['error'] = 'password';
+//             header("Location: login.php");
+//             exit;
+//         } else {
+//             if ($_POST['captcha'] !== $_SESSION['captcha']) {
+//                 $_SESSION['attempts']++;
+//                 $_SESSION['error'] = 'captcha';
+//                 header("Location: login.php");
+//                 exit;
+//             }
+//             unset($_SESSION['attempts']);
+//             $_SESSION["AUTH"] = $row;
+//             header("Location: login_check_2.php");
+//             exit;
+//         }
+//     }
+// } catch (PDOException $e) {
+//     echo $e->getMessage();
+// }
 session_start();
 $max_attempts = 3;
 if (!isset($_SESSION['attempts'])) {
@@ -36,6 +80,17 @@ try {
             }
             unset($_SESSION['attempts']);
             $_SESSION["AUTH"] = $row;
+
+            // 登入成功的紀錄
+            $user = $_SESSION["AUTH"]["user"];
+            $log_time = date('Y-m-d H:i:s');
+            $log_status = '登入成功';
+            $log_message = "使用者 {$user} 登入成功，時間為 {$log_time}";
+            $log_sql = "INSERT INTO login_log (user, time, status, message) 
+                        VALUES (?, ?, ?, ?)";
+            $log_stmt = $pdo->prepare($log_sql);
+            $log_stmt->execute([$user, $log_time, $log_status, $log_message]);
+            
             header("Location: login_check_2.php");
             exit;
         }
@@ -43,4 +98,15 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+// 登入失敗的紀錄
+$user = $_POST['user'];
+$log_time = date('Y-m-d H:i:s');
+$log_status = '登入失敗';
+$log_message = "使用者 {$user} 登入失敗，時間為 {$log_time}";
+$log_sql = "INSERT INTO login_log (user, time, status, message) 
+            VALUES (?, ?, ?, ?)";
+$log_stmt = $pdo->prepare($log_sql);
+$log_stmt->execute([$user, $log_time, $log_status, $log_message]);
+
 ?>
--- login_check_2.php
+++ login_check_2.php
@@ -1,3 +1,26 @@
+<?php
+// require_once("pdo.php");
+// session_start();
+// if(isset($_SESSION["AUTH"]["user"])){
+//     $user = $_SESSION["AUTH"]["user"];
+//     $log_time = date('Y-m-d H:i:s');
+//     $log_status = '登入成功';
+//     $log_message = "使用者 {$user} 登入成功，時間為 {$log_time}";
+//     $log_sql = "INSERT INTO login_log (user, time, status, message) 
+//                 VALUES (?, ?, ?, ?)";
+//     $log_stmt = $pdo->prepare($log_sql);
+//     $log_stmt->execute([$user, $log_time, $log_status, $log_message]);
+// } else {
+//     $user = $_POST['user'];
+//     $log_time = date('Y-m-d H:i:s');
+//     $log_status = '登入失敗';
+//     $log_message = "使用者 {$user} 登入失敗，時間為 {$log_time}";
+//     $log_sql = "INSERT INTO login_log (user, time, status, message) 
+//                 VALUES (?, ?, ?, ?)";
+//     $log_stmt = $pdo->prepare($log_sql);
+//     $log_stmt->execute([$user, $log_time, $log_status, $log_message]);
+// }
+?>
 <!DOCTYPE html>
 <html lang="zh-Hant-tw">
 
@@ -8,14 +31,14 @@
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/style.css">
     <style>
-        td {
-            width: 180px;
-            height: 180px;
-            border: 2px solid #fff;
-            text-align: center;
-            font-size: 24px;
-            cursor: pointer;
-        }
+    td {
+        width: 180px;
+        height: 180px;
+        border: 2px solid #fff;
+        text-align: center;
+        font-size: 24px;
+        cursor: pointer;
+    }
     </style>
     <title>二次驗證</title>
 </head>
@@ -39,6 +62,64 @@
     </div>
 </body>
 <script src="./js/jquery-3.6.3.min.js"></script>
-<script src="./js/function.js"></script>
+<!-- <script src="./js/function.js"></script> -->
+<script>
+function check() {
+    const selectedCells = [];
+    $("td").click(function() {
+        const index = $(this).data("id") - 1;
+        if (selectedCells.length < 2 && !selectedCells.includes(index)) {
+            selectedCells.push(index);
+            $(this).addClass("selected");
+        } else if (selectedCells.includes(index)) {
+            selectedCells.splice(selectedCells.indexOf(index), 1);
+            $(this).removeClass("selected");
+        }
+    });
+
+    $("#validate").click(function() {
+        if (selectedCells.length === 2) {
+            const row1 = Math.floor(selectedCells[0] / 2);
+            const col1 = selectedCells[0] % 2;
+            const row2 = Math.floor(selectedCells[1] / 2);
+            const col2 = selectedCells[1] % 2;
+            if ((row1 === row2 && Math.abs(col1 - col2) === 1) || (col1 === col2 && Math.abs(row1 - row2) ===
+                1)) {
+                $.ajax({
+                    url: "check_login.php",
+                    method: "POST",
+                    data: {
+                        status: "success"
+                    },
+                    success: function(response) {
+                        alert("登入成功！");
+                        location.href = "index.php";
+                    },
+                    error: function() {
+                        alert("發生錯誤！");
+                    },
+                });
+            } else {
+                $.ajax({
+                    url: "check_login.php",
+                    method: "POST",
+                    data: {
+                        status: "fail"
+                    },
+                    success: function(response) {
+                        alert("二次驗證錯誤！");
+                        location.href = "login.php";
+                    },
+                    error: function() {
+                        alert("發生錯誤！");
+                    },
+                });
+            }
+        }
+    });
+}
+
+check();
+</script>
 
 </html>
