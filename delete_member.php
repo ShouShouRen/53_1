bf71714bd92c7a70c5a796a67432ecd92175453d
Peter Chen <peterchen2017125@gmail.com>
2023-03-11 14:00:45 +0800

    20230311

--- delete_member.php
+++ delete_member.php
@@ -1,7 +1,7 @@
 <?php
     require_once("pdo.php");
     extract($_GET);
-    $sql = "DELETE * FROM users WHERE id = {$id}";
+    $sql = "DELETE FROM users WHERE id = {$id}";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     header("Location:member_list.php");
